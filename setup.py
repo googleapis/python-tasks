f976544bb205cc51c74168c3afada107de9538a3
Bu Sun Kim <busunkim@google.com>
2022-03-11 02:00:23 +0000

    chore(deps): allow google-api-core v2 on v1 release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ description = "Cloud Tasks API API client library"
 version = "1.5.0"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
+    "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
     "grpc-google-iam-v1 >= 0.12.3, < 0.13dev",
     'enum34; python_version < "3.4"',
 ]
